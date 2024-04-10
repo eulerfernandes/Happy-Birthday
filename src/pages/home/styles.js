0285f42fe45554c3817fc8cbd7108a72ba584cfb
rogerfernandez23 <rogerio.fernandes2212@gmail.com>
2024-04-09 21:41:25 -0300

    update-home

---
+++ src/pages/home/styles.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+
+export const Container = styled.div`
+
+padding: 50px;
+`;
+
+export const ContainerLinks = styled.div`
+display: flex;
+flex-direction: row;
+gap: 100px;
+
+
+    .box {
+        display: flex;
+        gap: 20px;
+    }
+
+`;
+
+export const ContainerItens = styled.div``;
+
