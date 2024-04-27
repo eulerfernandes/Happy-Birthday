c970022ab0e5cec29e2824bc4b062e29a72a2237
rogerfernandez23 <rogerio.fernandes2212@gmail.com>
2024-04-27 10:56:37 -0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
-import './index.css';
-import App from './App';
-=======
-import Home from './pages/home';
 import GlobalStyles from './styles/globalStyles';
->>>>>>> 0285f42fe45554c3817fc8cbd7108a72ba584cfb
+import Home from './pages/home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,7 +10,3 @@ root.render(
         <Home /> 
     </>
 );
-<<<<<<< HEAD
-
-=======
->>>>>>> 0285f42fe45554c3817fc8cbd7108a72ba584cfb
