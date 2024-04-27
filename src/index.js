5e6028ea6bf9a4281d9d709f798540678b89bbae
Katiane <almeidakatiane29@gmail.com>
2024-04-27 19:20:48 -0300

    index.js update

--- src/index.js
+++ src/index.js
@@ -2,10 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Home from './pages/home';
 import GlobalStyles from './styles/globalStyles';
-<<<<<<< HEAD
 import Second from './pages/second';
-=======
->>>>>>> ac523953bc39cb72e217be226f5732430009b996
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
