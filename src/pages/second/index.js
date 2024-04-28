df58dc848c3e87d485dd5fc89344dac1c5919094
Katiane <almeidakatiane29@gmail.com>
2024-04-28 14:02:19 -0300

    status

--- src/pages/second/index.js
+++ src/pages/second/index.js
@@ -4,24 +4,15 @@ import img from '../../assets/img 1.png'
 import img1 from '../../assets/img 2.png'
 import img2 from '../../assets/img 3.png'
 
-
-
-
 function Second() {
     return (
-        
         <Container>
-        
         <div> 
             <img src={img}></img>
             <img src={img1}></img>
             <img src={img2}></img>
-        </div> 
-
+        </div>
         </Container>
-
-
-
     )
 }
 
